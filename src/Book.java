0decb7512e48e9ae08064c7dd958d7f799224140
Bakir2u2 <abakes1@unlv.nevada.edu>
2026-04-12 13:12:17 -0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,15 @@
 public class Book {
-}
+      // PROPERTIES
+    private String title;
+    private String author;
+    private String ISBN;
+    private int publicationYear;
+    private boolean isAvailable;
+  // CONSTRUCTOR
+    public Book(String title, String author, String ISBN, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.publicationYear = publicationYear;
+        this.isAvailable = true; // book is available when created
+    }
