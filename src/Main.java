4b7cce95bbf3d9ccadb887f4e4874f149495fde5
Anthony <paynea14@unlv.nevada.edu>
2026-04-12 11:55:15 -0700

    Added code to Main Added more text to README

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Start of Library Database");
+
+    public static void main(String[] args) {
+
+        //initial setup
+        Library library = new Library();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        System.out.println("Welcome to our Library!");
+
+
+        //Main Loop starts here
     }
 }
